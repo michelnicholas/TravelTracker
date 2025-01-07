8fddd8284e9e9087e9f117d63fb2887ff2072f97
Michel Nicholas <michelnicholas1993@gmail.com>
2025-01-07 15:13:20 -0500

    Working as expected

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/add", async (req, res) => {
 
   try {
     const result = await db.query(
-      "Select country_code FROM countries WHERE LOWER(name) LIKE '%' || $1 || '%';",
+      "SELECT country_code FROM countries WHERE country_name = $1",
       [input]
     );
 
